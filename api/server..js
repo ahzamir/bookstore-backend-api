ac65e8fddf3edc07177ff6702b7d81c766c414e8
ahzamir <ahmadzamiryousofi1397@gmail.com>
2024-11-03 00:10:58 +0100

    added the files for the deployment

---
+++ api/server..js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('../config/db');
+const userRoutes = require('../routes/userRoutes');
+const bookRoutes = require('../routes/bookRouters');
+
+const app = express();
+app.use(express.json());
+connectDB();
+
+app.use('/api/users', userRoutes);
+app.use('/api/books', bookRoutes);
+
+// Export the app for Vercel as a serverless function
+module.exports = app;
