5aac101e5d5254a6162c8d61d5ff698345037e67
ahzamir <ahmadzamiryousofi1397@gmail.com>
2024-11-16 00:56:33 +0100

    added some changes

--- api/index.js
+++ api/index.js
@@ -1,13 +1,21 @@
-import express from 'express';
-
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+const bookRoutes = require('./routes/bookRouters');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Express API is working on vercel');
+app.use(express.json());
+connectDB();
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
 });
 
-app.listen(5000, () => {
-    console.log('Server running on PORT 5000');
-});
+app.use('/api/users', userRoutes);
+app.use('/api/books', bookRoutes);
 
-export default app;
+app.listen(5000, () => console.log('Server running on PORT 5000'));
