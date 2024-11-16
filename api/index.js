4d383362eb2445ac8e31a1b1cc91b6c10a045ea0
ahzamir <ahmadzamiryousofi1397@gmail.com>
2024-11-16 01:40:42 +0100

    added the vercel config to the app

--- api/index.js
+++ api/index.js
@@ -10,17 +10,10 @@ const app = express();
 app.use(express.json());
 connectDB();
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
-
-app.use('/', (req, res) => {
-    res.send('Welcome to Bookstore API');
-});
+app.get("/", (req, res) => res.send("Express on Vercel"));
 app.use('/api/users', userRoutes);
 app.use('/api/books', bookRoutes);
 
-app.listen(5000, () => console.log('Server running on PORT 5000'));
+app.listen(3000, () => console.log('Server running on PORT 3000'));
+
+export default app;
